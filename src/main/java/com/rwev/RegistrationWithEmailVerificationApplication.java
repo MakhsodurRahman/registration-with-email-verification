e415a7b8213f55faa6862928f16306320561b33b
Makhsodur-Rahman <makhsodurrahmanhridoy@gmail.com>
2022-09-28 00:31:23 +0600

    project created

---
+++ src/main/java/com/rwev/RegistrationWithEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.rwev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationWithEmailVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationWithEmailVerificationApplication.class, args);
+    }
+
+}
