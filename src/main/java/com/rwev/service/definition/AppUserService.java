c96e1da0e230664b0add044d0ae2cd4b74021058
Makhsodur-Rahman <makhsodurrahmanhridoy@gmail.com>
2022-09-28 18:44:49 +0600

    duplicate email validation

--- src/main/java/com/rwev/service/definition/AppUserService.java
+++ src/main/java/com/rwev/service/definition/AppUserService.java
@@ -1,8 +1,12 @@
 package com.rwev.service.definition;
 
+import com.rwev.entity.AppUser;
+import com.rwev.enums.AppUserRole;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface AppUserService extends UserDetailsService {
     public UserDetails loadUserByUsername(String username);
+    public String signUp(AppUser appUser);
+
 }
--- src/main/java/com/rwev/service/implementation/AppUserServiceImpl.java
+++ src/main/java/com/rwev/service/implementation/AppUserServiceImpl.java
@@ -1,21 +1,36 @@
 package com.rwev.service.implementation;
 
+import com.rwev.entity.AppUser;
 import com.rwev.repository.AppUserRepository;
 import com.rwev.service.definition.AppUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class AppUserServiceImpl implements AppUserService {
 
     private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email).orElseThrow(()->new UsernameNotFoundException("user not found"));
+    }
+
+    public String signUp(AppUser appUser){
+      boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+      if(userExists){
+          throw new IllegalStateException("email already taken");
+      }
+      appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
+      appUserRepository.save(appUser);
+        return "user save database";
     }
 }
