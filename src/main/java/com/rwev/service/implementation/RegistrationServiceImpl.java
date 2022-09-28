ee0b3391adcd5cba1aec3b45d72008a05a3973e9
Makhsodur-Rahman <makhsodurrahmanhridoy@gmail.com>
2022-09-28 18:45:23 +0600

    user registration

--- src/main/java/com/rwev/service/implementation/RegistrationServiceImpl.java
+++ src/main/java/com/rwev/service/implementation/RegistrationServiceImpl.java
@@ -1,7 +1,11 @@
 package com.rwev.service.implementation;
 
 import com.rwev.dto.RegistrationReqDto;
+import com.rwev.entity.AppUser;
+import com.rwev.enums.AppUserRole;
 import com.rwev.repository.AppUserRepository;
+import com.rwev.service.EmailValidator;
+import com.rwev.service.definition.AppUserService;
 import com.rwev.service.definition.RegistrationService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,10 +15,21 @@ import org.springframework.stereotype.Service;
 public class RegistrationServiceImpl implements RegistrationService {
 
     private final AppUserRepository appUserRepository;
+    private final EmailValidator emailValidator;
+    private final AppUserService appUserService;
     @Override
-    public String register(RegistrationReqDto registrationReqDto) {
+    public String register(RegistrationReqDto request) {
 
-
-        return "work";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUp(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+        ));
     }
 }
